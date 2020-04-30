c5f5742180e0633ed1a4135b9c0b60a63759fd7d
MaJiaju <majiaju96@163.com>
2020-04-30 15:08:53 +0800

    SpringBoot搭建博客框架成功

---
+++ src/main/java/com/majiaju/web/IndexController.java
@@ -0,0 +1,19 @@
+package com.majiaju.web;
+
+import com.majiaju.NotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ *
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+
+        return "index";
+    }
+}
